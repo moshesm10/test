dc0ab3f1a233723991740512611d73437052a71b
Alex <moshesm@yandex.ru>
2021-02-03 23:34:00 +0300

    2

--- index.php
+++ index.php
@@ -9,9 +9,8 @@
             or die("Ошибка " . mysqli_error($link));
 
 
-        $query = "INSERT INTO `test` (`data`) VALUES $_REQUEST;";
+        $query = "INSERT INTO `test` (`data`) VALUES $_REQUEST";
         $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
         // close link
         mysqli_close($link);
-
         ?>
