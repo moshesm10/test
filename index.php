7196b88d7ee6c34489ac9c5b541834796a36bc12
Alex <moshesm@yandex.ru>
2021-02-03 23:50:06 +0300

    9

--- index.php
+++ index.php
@@ -13,4 +13,11 @@
         // close link
 
         mysqli_close($link);
+        if($json = json_decode(file_get_contents("php://input"), true)) {
+            print_r($json);
+            $data = $json;
+        } else {
+            print_r($_POST);
+            $data = $_POST;
+        }
         ?>
