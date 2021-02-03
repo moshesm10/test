a252f10a4e8e52cd0e6440eae0293cb552cf6ca0
Alex <moshesm@yandex.ru>
2021-02-03 23:30:00 +0300

    1

---
+++ index.php
@@ -0,0 +1,17 @@
+        <?php 
+         
+        $user = 'a0413857_test';
+        $password = 'test';
+        $db = 'a0413857_test';
+        $host = 'localhost';
+
+        $link = mysqli_connect($host, $user, $password, $db) 
+            or die("Ошибка " . mysqli_error($link));
+
+
+        $query = "INSERT INTO `test` (`data`) VALUES $_REQUEST;";
+        $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+        // close link
+        mysqli_close($link);
+
+        ?>
